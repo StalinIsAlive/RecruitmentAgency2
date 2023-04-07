95464ee6b5ce4e0c6a0cac95f2fc72acf20b8f03
Artem <foxymilyan_jiuc@list.ru>
2023-04-07 23:14:45 +0300

    Создание проекта и реализация подключения к БД

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace recruitment_agency.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
